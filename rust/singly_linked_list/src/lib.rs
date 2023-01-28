2f9a512c98c8842892500bb4e41f1799de7e13b9
Ajlow2000 <ajlow2000@gmail.com>
2023-01-28 12:42:05 -0600

    Adding basic files with gitignore fix

--- rust/singly_linked_list/src/lib.rs
+++ rust/singly_linked_list/src/lib.rs
@@ -1,5 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+struct Node<T> {
+    key: T,
+    next: Box<Node<T>>,
+}
+
+pub struct SinglyLinkedList<T> {
+    head: Box<Node<T>>,
+    length: usize,
+}
+
+impl<T> SinglyLinkedList<T> {
+    pub fn new() -> Self {
+        Self { head: Box, length: 0 }
+    }
+
+    pub fn push(&mut self, key: T) {
+        unimplemented!();
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        unimplemented!();
+    }
+
+    pub fn get_length(&self) {
+        return self.length;
+    }
 }
 
 #[cfg(test)]
