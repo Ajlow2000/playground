cac69cc614885410765ec2bda18321e0bfb6f87b
ajlow2000 <ajlow2000@gmail.com>
2024-05-16 17:02:52 +0000

    build(c): Add flake and proj structure

---
+++ c/flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "C/C++ environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }@inputs:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        p = import nixpkgs { inherit system; };
+        llvm = p.llvmPackages_latest;
+     in
+      {
+        devShell = p.mkShell.override { stdenv = p.clangStdenv; } rec {
+          packages = with p; [
+            # builder
+            gcc
+            just
+
+            # debugger
+            llvm.lldb
+            gdb
+
+            # fix headers not found
+            clang-tools
+
+            # LSP and compiler
+            llvm.libstdcxxClang
+
+            # other tools
+            llvm.libllvm
+            valgrind
+
+            # libs
+            glm
+            SDL2
+            SDL2_gfx
+          ];
+          name = "C";
+        };
+      }
+    );
+}
+
