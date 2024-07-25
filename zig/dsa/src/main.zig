fff36d0155fe1f6c9f6a5de9f43c9903c5ea884e
ajlow2000 <ajlow2000@gmail.com>
2024-07-25 20:06:06 +0000

    WIP(prog save):

--- zig/dsa/src/main.zig
+++ zig/dsa/src/main.zig
@@ -1,8 +1,18 @@
 const std = @import("std");
 const sll = @import("singlyLinkedList");
 
-const singlyLinkedListU16 = sll.SinglyLinkedList(u16);
+const SinglyLinkedListU8 = sll.SinglyLinkedList(u8);
 
 pub fn main() !void {
     std.debug.print("Hello, World!\n", .{});
+
+    var list = SinglyLinkedListU8{};
+
+    var first = SinglyLinkedListU8.Node{ .key = 1 };
+    var second = SinglyLinkedListU8.Node{ .key = 2 };
+    var third = SinglyLinkedListU8.Node{ .key = 3 };
+
+    try list.insertHead(&first);
+    try list.insertHead(&second);
+    try list.insertHead(&third);
 }
