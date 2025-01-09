83af14162fec228c7a252576f6f3731eb9f15768
ajlow2000 <ajlow2000@gmail.com>
2025-01-09 17:22:11 +0000

    wip: cant get zig-cli to work here

--- zig/hello/build.zig
+++ zig/hello/build.zig
@@ -29,12 +29,17 @@ pub fn build(b: *std.Build) void {
     // running `zig build`).
     b.installArtifact(lib);
 
+    const zigcli_dep = b.dependency("zig-cli", .{ .target = target });
+    const zigcli_mod = zigcli_dep.module("zig-cli");
+
     const exe = b.addExecutable(.{
         .name = "hello",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    exe.root_module.addImport("zig-cli", zigcli_mod);
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
