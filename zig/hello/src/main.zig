4685cc6d566b797efc21ae4372ef8a74d26293f1
ajlow2000 <ajlow2000@gmail.com>
2025-01-11 10:45:55 -0700

    WIP(broken)!: brokwn
    BREAKING CHANGE:

--- zig/hello/src/main.zig
+++ zig/hello/src/main.zig
@@ -1,20 +1,74 @@
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    // First we specify what parameters our program can take.
+    // We can use `parseParamsComptime` to parse a string into an array of `Param(Help)`.
+    const params = comptime clap.parseParamsComptime(
+        \\-h, --help             Display this help and exit.
+        \\-n, --number <INT>     An option parameter, which takes a value.
+        \\-a, --answer <ANSWER>  An option parameter which takes an enum.
+        \\-s, --string <STR>...  An option parameter which can be specified multiple times.
+        \\<FILE>...
+        \\
+    );
+
+    // Declare our own parsers which are used to map the argument strings to other
+    // types.
+    const YesNo = enum { yes, no };
+    const parsers = comptime .{
+        .STR = clap.parsers.string,
+        .FILE = clap.parsers.string,
+        .INT = clap.parsers.int(usize, 10),
+        .ANSWER = clap.parsers.enumeration(YesNo),
+    };
+
+    var diag = clap.Diagnostic{};
+    var res = clap.parse(clap.Help, &params, parsers, .{
+        .diagnostic = &diag,
+        .allocator = gpa.allocator(),
+        // The assignment separator can be configured. `--number=1` and `--number:1` is now
+        // allowed.
+        .assignment_separators = "=:",
+    }) catch |err| {
+        diag.report(std.io.getStdErr().writer(), err) catch {};
+        return err;
+    };
+    defer res.deinit();
+
+    if (res.args.help != 0)
+        std.debug.print("--help\n", .{});
+    if (res.args.number) |n|
+        std.debug.print("--number = {}\n", .{n});
+    if (res.args.answer) |a|
+        std.debug.print("--answer = {s}\n", .{@tagName(a)});
+    for (res.args.string) |s|
+        std.debug.print("--string = {s}\n", .{s});
+    for (res.positionals[0]) |pos|
+        std.debug.print("{s}\n", .{pos});
+}
+
+const clap = @import("clap");
 const std = @import("std");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
+// const std = @import("std");
+//
+// pub fn main() !void {
+//     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+//     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+//
+//     // stdout is for the actual output of your application, for example if you
+//     // are implementing gzip, then only the compressed bytes should be sent to
+//     // stdout, not any debugging messages.
+//     const stdout_file = std.io.getStdOut().writer();
+//     var bw = std.io.bufferedWriter(stdout_file);
+//     const stdout = bw.writer();
+//
+//     try stdout.print("Run `zig build test` to run the tests.\n", .{});
+//
+//     try bw.flush(); // don't forget to flush!
+//
+// }
 
 test "simple test" {
     var list = std.ArrayList(i32).init(std.testing.allocator);
