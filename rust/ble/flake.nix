c9540d3d3244ac7b03146d04fb5a7778ebaa894d
Alec Lowry <accounts@aleclowry.com>
2025-07-13 08:16:51 -0600

    feat: ble crate init

---
+++ rust/ble/flake.nix
@@ -0,0 +1,60 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    outputs = inputs@{ self, ... }: with inputs;
+        let
+           forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.unix;
+           nixpkgsFor = forAllSystems (system: import nixpkgs {
+                inherit system;
+                config = { };
+            });
+            cargoToml = nixpkgs.lib.importTOML ./Cargo.toml;
+
+        in {
+            packages = forAllSystems (system:
+                let pkgs = nixpkgsFor."${system}"; in {
+                    default = pkgs.rustPlatform.buildRustPackage {
+                        pname = cargoToml.package.name;
+                        version = cargoToml.package.version;
+                        src = ./.;
+                        cargoHash = "sha256-3kzJIZw6qyvyxfyzhuPU0HwhrMJd/z4x9nOmzLfhiHo=";
+                    };
+                }
+            );
+            checks = forAllSystems (system:
+                let pkgs = nixpkgsFor."${system}"; in {
+                    default = pkgs.rustPlatform.buildRustPackage {
+                        pname = cargoToml.package.name + "-tests";
+                        version = cargoToml.package.version;
+                        src = ./.;
+                        cargoHash = "sha256-3kzJIZw6qyvyxfyzhuPU0HwhrMJd/z4x9nOmzLfhiHo=";
+                        checkPhase = ''
+                            cargo test
+                        '';
+                        installPhase = ''
+                            touch $out
+                        '';
+                    };
+                }
+            );
+            apps = forAllSystems (system: {
+                default = {
+                    type = "app";
+                    program = "${self.packages.${system}.default}/bin/${cargoToml.package.name}";
+                };
+            });
+            devShells = forAllSystems (system:
+                let pkgs = nixpkgsFor."${system}"; in {
+                    default = pkgs.mkShell {
+                        packages = with pkgs; [
+                            rustc
+                            cargo
+                            rust-analyzer
+                        ];
+                    };
+                }
+            );
+       };
+}
