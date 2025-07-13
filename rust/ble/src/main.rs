4a5ef46a6f379c9f7c980cb95efc606c5614cd60
Alec Lowry <accounts@aleclowry.com>
2025-07-13 10:04:54 -0600

    feat: trim down main to only do discover

--- rust/ble/src/main.rs
+++ rust/ble/src/main.rs
@@ -1,6 +1,6 @@
 // copied from https://github.com/deviceplug/btleplug/blob/master/examples/event_driven_discovery.rs
 use btleplug::api::{
-    Central, CentralEvent, Manager as _, Peripheral, ScanFilter,
+    Central, CentralEvent, Manager as _, ScanFilter,
 };
 use btleplug::platform::{Adapter, Manager};
 use futures::stream::StreamExt;
@@ -16,57 +16,19 @@ async fn main() -> anyhow::Result<()> {
 
     let manager = Manager::new().await?;
 
-    // get the first bluetooth adapter
-    // connect to the adapter
     let central = get_central(&manager).await;
 
     let central_state = central.adapter_state().await.unwrap();
     println!("CentralState: {:?}", central_state);
 
-    // Each adapter has an event stream, we fetch via events(),
-    // simplifying the type, this will return what is essentially a
-    // Future<Result<Stream<Item=CentralEvent>>>.
     let mut events = central.events().await?;
 
-    // start scanning for devices
     central.start_scan(ScanFilter::default()).await?;
 
-    // Print based on whatever the event receiver outputs. Note that the event
-    // receiver blocks, so in a real program, this should be run in its own
-    // thread (not task, as this library does not yet use async channels).
     while let Some(event) = events.next().await {
         match event {
             CentralEvent::DeviceDiscovered(id) => {
-                // let peripheral = central.peripheral(&id).await?;
-                // let properties = peripheral.properties().await?;
-                // let name = properties
-                //     .and_then(|p| p.local_name)
-                //     .map(|local_name| format!("Name: {local_name}"))
-                //     .unwrap_or_default();
                 println!("DeviceDiscovered: {}", id.to_string());
-            }
-            CentralEvent::StateUpdate( .. ) => {
-                // println!("AdapterStatusUpdate {:?}", state);
-            }
-            CentralEvent::DeviceConnected(id) => {
-                println!("DeviceConnected: {:?}", id);
-            }
-            CentralEvent::DeviceDisconnected(id) => {
-                println!("DeviceDisconnected: {:?}", id);
-            }
-            CentralEvent::ManufacturerDataAdvertisement { .. } => {
-                // println!(
-                //     "ManufacturerDataAdvertisement: {:?}, {:?}",
-                //     id, manufacturer_data
-                // );
-            }
-            CentralEvent::ServiceDataAdvertisement { .. } => {
-                // println!("ServiceDataAdvertisement: {:?}, {:?}", id, service_data);
-            }
-            CentralEvent::ServicesAdvertisement { .. } => {
-                // let services: Vec<String> =
-                //     services.into_iter().map(|s| s.to_short_string()).collect();
-                // println!("ServicesAdvertisement: {:?}, {:?}", id, services);
             }
             _ => {}
         }
