591955cfcf07560ce91bc4a2d84a5817027d4d3a
Alec Lowry <accounts@aleclowry.com>
2025-07-17 20:13:35 -0600

    feat: add basics for rb tree in rust. SoonTM

---
+++ rust/rb_tree/flake.nix
@@ -0,0 +1,65 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    outputs = inputs@{ self, ... }: with inputs;
+        let
+           forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.unix;
+           nixpkgsFor = forAllSystems (system: import nixpkgs {
+                inherit system;
+                config = { };
+            });
+            cargoToml = nixpkgs.lib.importTOML ./Cargo.toml;
+        in {
+            packages = forAllSystems (system:
+                let pkgs = nixpkgsFor."${system}"; in {
+                    default = pkgs.rustPlatform.buildRustPackage {
+                        pname = cargoToml.package.name;
+                        version = cargoToml.package.version;
+                        src = ./.;
+                        cargoHash = "sha256-uplp1vbvQv9VBxrhBOFWHWASJo/jrh3bO+lAN3y0vX0=";
+                        nativeBuildInputs = with pkgs; [ pkg-config ];
+                        buildInputs = with pkgs; [ dbus ];
+                    };
+                }
+            );
+            checks = forAllSystems (system:
+                let pkgs = nixpkgsFor."${system}"; in {
+                    default = pkgs.rustPlatform.buildRustPackage {
+                        pname = cargoToml.package.name + "-tests";
+                        version = cargoToml.package.version;
+                        src = ./.;
+                        cargoHash = "sha256-uplp1vbvQv9VBxrhBOFWHWASJo/jrh3bO+lAN3y0vX0=";
+                        nativeBuildInputs = with pkgs; [ pkg-config ];
+                        buildInputs = with pkgs; [ dbus ];
+                        checkPhase = ''
+                            cargo test
+                        '';
+                        installPhase = ''
+                            touch $out
+                        '';
+                    };
+                }
+            );
+            apps = forAllSystems (system: {
+                default = {
+                    type = "app";
+                    program = "${self.packages.${system}.default}/bin/${cargoToml.package.name}";
+                };
+            });
+            devShells = forAllSystems (system:
+                let pkgs = nixpkgsFor."${system}"; in {
+                    default = pkgs.mkShell {
+                        packages = with pkgs; [
+                            pkg-config
+                            dbus
+                            rustc
+                            cargo
+                            rust-analyzer
+                        ];
+                    };
+                }
+            );
+       };
+}
