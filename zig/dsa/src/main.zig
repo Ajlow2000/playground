245425b93fc4d414c0ca3b269f4b5ad2d664ae21
Alec Lowry <accounts@aleclowry.com>
2025-08-21 16:07:55 -0600

    feat: add heap allocation

--- zig/dsa/src/main.zig
+++ zig/dsa/src/main.zig
@@ -4,16 +4,30 @@ const sll = @import("singlyLinkedList");
 const SinglyLinkedListU8 = sll.SinglyLinkedList(u8);
 
 pub fn main() !void {
-    std.debug.print("Hello, World!\n", .{});
+    std.debug.print("Stress testing SinglyLinkedListU8\n", .{});
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+
+    const n = 10000;
 
     var list = SinglyLinkedListU8{};
 
-    var first = SinglyLinkedListU8.Node{ .key = 1 };
-    var second = SinglyLinkedListU8.Node{ .key = 2 };
-    var third = SinglyLinkedListU8.Node{ .key = 3 };
+    for (0..n) |i| {
+        std.debug.print("Inserting node {}.\n", .{i});
+        const node = try alloc.create(SinglyLinkedListU8.Node);
+        node.* = SinglyLinkedListU8.Node{ .key = std.crypto.random.int(u8) };
+        try list.insertHead(node);
+    }
 
-    try list.insertHead(&first);
-    try list.insertHead(&second);
-    try list.insertHead(&third);
     std.debug.print("list - {pretty}\n", .{list});
+
+    // Cleanup: iterate through list nodes and call alloc.destroy()
+    var current = list.head;
+    while (current) |node| {
+        const next = node.next;
+        alloc.destroy(node);
+        current = next;
+    }
 }
