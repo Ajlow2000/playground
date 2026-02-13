b2421e8b4bf84b8bc3b1571bd6d7d934446f92de
Alec Lowry <accounts@aleclowry.com>
2026-02-13 12:02:22 -0700

    feat: ada hw

---
+++ ada/helloWorld/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Hello World in Ada";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "hello-world-ada";
+          src = ./.;
+          nativeBuildInputs = [ pkgs.gnat pkgs.gprbuild ];
+          buildPhase = ''
+            gprbuild -P hello_world.gpr
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp bin/hello $out/bin/
+          '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.gnat pkgs.gprbuild ];
+        };
+      });
+}
