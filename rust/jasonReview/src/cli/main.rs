c081b315a008c1ef6f9bf7b4816f1674c2e18906
Alec Lowry <accounts@aleclowry.com>
2026-03-31 15:35:07 -0600

    review: attach struct to job variant directly
    idk if this diff will be useful to you at all since I dont undestand the
    problem space here really.  But things felt off to me so I just moved
    things around till it felt better.
    
    All this to say, idk if this is semantically accurate, but you can
    attach structs to enum variants.
    
    This diff might only be valuable if we chat about it

--- rust/jasonReview/src/cli/main.rs
+++ rust/jasonReview/src/cli/main.rs
@@ -2,30 +2,16 @@ use std::collections::VecDeque;
 use std::time::Duration;
 
 #[derive(Clone, Debug)]
-enum EventType {
-    Job,
-    #[allow(dead_code)]
-    NewJob,
+enum Event {
+    Job {
+        total_time: Duration,
+        time_so_far: Duration,
+        n_slices: i32,
+    },
 }
 
-#[derive(Clone, Debug)]
-struct QMember {
-    #[allow(dead_code)]
-    event_type: EventType,
-    #[allow(dead_code)]
-    serial: i32,
-    #[allow(dead_code)]
-    start_time: Duration,
-    total_time: Duration,
-    time_so_far: Duration,
-    n_slices: i32,
-}
-
-fn add_new_event(queue: &mut VecDeque<QMember>, event_type: EventType, total_time: Duration, m_serial: i32, m_time: Duration) -> i32 {
-    queue.push_back(QMember {
-        event_type,
-        serial: m_serial,
-        start_time: m_time,
+fn add_new_event(queue: &mut VecDeque<Event>, total_time: Duration, m_serial: i32) -> i32 {
+    queue.push_back(Event::Job {
         total_time,
         time_so_far: Duration::ZERO,
         n_slices: 0,
@@ -33,9 +19,11 @@ fn add_new_event(queue: &mut VecDeque<QMember>, event_type: EventType, total_tim
     m_serial + 1
 }
 
-fn list_queue(current_time: Duration, queue: &VecDeque<QMember>) {
+fn list_queue(current_time: Duration, queue: &VecDeque<Event>) {
     let time_remaining: Duration = queue.iter()
-        .map(|m| m.total_time - m.time_so_far)
+        .map(|e| match e {
+            Event::Job { total_time, time_so_far, .. } => *total_time - *time_so_far,
+        })
         .sum();
     println!("{:7.2}	jobs: {}	remaining: {:7.2}", current_time.as_secs_f32(), queue.len(), time_remaining.as_secs_f32());
 }
@@ -47,12 +35,12 @@ fn main() {
 
     let mut m_serial: i32 = 0;
     let mut m_time = Duration::ZERO;
-    let mut q: VecDeque<QMember> = VecDeque::new();
+    let mut q: VecDeque<Event> = VecDeque::new();
     let mut next_job_arrival = Duration::ZERO;
 
     for _i in 1..10 {
         let run_time = Duration::from_secs_f32(rand::random_range(2.0_f32..20.0));
-        m_serial = add_new_event(&mut q, EventType::Job, run_time, m_serial, m_time);
+        m_serial = add_new_event(&mut q, run_time, m_serial);
         println!("addedEvent {m_serial}");
     }
 
@@ -61,19 +49,23 @@ fn main() {
     while m_time < time_window && !q.is_empty() {
         if m_time > next_job_arrival {
             let run_time = Duration::from_secs_f32(rand::random_range(2.0_f32..20.0));
-            m_serial = add_new_event(&mut q, EventType::Job, run_time, m_serial, m_time);
+            m_serial = add_new_event(&mut q, run_time, m_serial);
             next_job_arrival += Duration::from_secs(20);
         }
         let mut job = q.pop_front().expect("queue was empty");
-        if (job.total_time - job.time_so_far) > time_slice {
-            job.time_so_far += time_slice;
-            job.n_slices += 1;
+        let remaining = match &job {
+            Event::Job { total_time, time_so_far, .. } => *total_time - *time_so_far,
+        };
+        if remaining > time_slice {
+            let Event::Job { time_so_far, n_slices, .. } = &mut job;
+            *time_so_far += time_slice;
+            *n_slices += 1;
             q.push_back(job);
             m_time += time_slice + task_switch_overhead;
         } else {
-            m_time += (job.total_time - job.time_so_far) + task_switch_overhead;
-            job.time_so_far = job.total_time;
-            println!("finished job - expended {:.2}", job.total_time.as_secs_f32());
+            m_time += remaining + task_switch_overhead;
+            let Event::Job { total_time, .. } = job;
+            println!("finished job - expended {:.2}", total_time.as_secs_f32());
         }
         list_queue(m_time, &q);
     }
