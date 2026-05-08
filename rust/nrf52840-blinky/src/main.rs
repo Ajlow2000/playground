659c20a1d84ab57a87df288075096d0bf883f915
Alec Lowry <accounts@aleclowry.com>
2026-05-07 21:05:23 -0600

    feat: async button presses wow

--- rust/nrf52840-blinky/src/main.rs
+++ rust/nrf52840-blinky/src/main.rs
@@ -3,9 +3,21 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_nrf::gpio::{Input, Level, Output, OutputDrive, Pull};
-use embassy_time::Timer;
+use embassy_futures::join::join4;
+use embassy_nrf::gpio::{Level, Output, OutputDrive, Pull};
+use embassy_nrf::gpiote::{InputChannel, InputChannelPolarity};
 use {defmt_rtt as _, panic_probe as _};
+
+async fn watch_button(mut btn: InputChannel<'_>, led: &mut Output<'_>, id: u8) {
+    loop {
+        btn.wait_for_low().await;
+        info!("Button {} pressed", id);
+        led.set_low();
+        btn.wait_for_high().await;
+        debug!("Button {} released", id);
+        led.set_high();
+    }
+}
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -16,32 +28,18 @@ async fn main(_spawner: Spawner) {
     let mut led3 = Output::new(p.P0_15, Level::High, OutputDrive::Standard);
     let mut led4 = Output::new(p.P0_16, Level::High, OutputDrive::Standard);
 
-    let btn1 = Input::new(p.P0_11, Pull::Up);
-    let btn2 = Input::new(p.P0_12, Pull::Up);
-    let btn3 = Input::new(p.P0_24, Pull::Up);
-    let btn4 = Input::new(p.P0_25, Pull::Up);
+    let btn1 = InputChannel::new(p.GPIOTE_CH0, p.P0_11, Pull::Up, InputChannelPolarity::Toggle);
+    let btn2 = InputChannel::new(p.GPIOTE_CH1, p.P0_12, Pull::Up, InputChannelPolarity::Toggle);
+    let btn3 = InputChannel::new(p.GPIOTE_CH2, p.P0_24, Pull::Up, InputChannelPolarity::Toggle);
+    let btn4 = InputChannel::new(p.GPIOTE_CH3, p.P0_25, Pull::Up, InputChannelPolarity::Toggle);
 
-    info!("Ready");
+    info!("Application Ready -- Launching...");
 
-    let mut prev = [false; 4];
-    loop {
-        let curr = [btn1.is_low(), btn2.is_low(), btn3.is_low(), btn4.is_low()];
-
-        for i in 0..4 {
-            if curr[i] != prev[i] {
-                if curr[i] {
-                    info!("Button {} pressed", i + 1);
-                } else {
-                    debug!("Button {} released", i + 1);
-                }
-            }
-        }
-        prev = curr;
-
-        led1.set_level(if curr[0] { Level::Low } else { Level::High });
-        led2.set_level(if curr[1] { Level::Low } else { Level::High });
-        led3.set_level(if curr[2] { Level::Low } else { Level::High });
-        led4.set_level(if curr[3] { Level::Low } else { Level::High });
-        Timer::after_millis(10).await;
-    }
+    join4(
+        watch_button(btn1, &mut led1, 1),
+        watch_button(btn2, &mut led2, 2),
+        watch_button(btn3, &mut led3, 3),
+        watch_button(btn4, &mut led4, 4),
+    )
+    .await;
 }
