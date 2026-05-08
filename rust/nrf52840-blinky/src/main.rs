9db3e608e6a1924581c07f107a8421ac3848e844
Alec Lowry <accounts@aleclowry.com>
2026-05-07 20:52:11 -0600

    feat: logging

--- rust/nrf52840-blinky/src/main.rs
+++ rust/nrf52840-blinky/src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use defmt::*;
 use embassy_executor::Spawner;
 use embassy_nrf::gpio::{Input, Level, Output, OutputDrive, Pull};
 use embassy_time::Timer;
@@ -20,11 +21,27 @@ async fn main(_spawner: Spawner) {
     let btn3 = Input::new(p.P0_24, Pull::Up);
     let btn4 = Input::new(p.P0_25, Pull::Up);
 
+    info!("Ready");
+
+    let mut prev = [false; 4];
     loop {
-        led1.set_level(if btn1.is_low() { Level::Low } else { Level::High });
-        led2.set_level(if btn2.is_low() { Level::Low } else { Level::High });
-        led3.set_level(if btn3.is_low() { Level::Low } else { Level::High });
-        led4.set_level(if btn4.is_low() { Level::Low } else { Level::High });
+        let curr = [btn1.is_low(), btn2.is_low(), btn3.is_low(), btn4.is_low()];
+
+        for i in 0..4 {
+            if curr[i] != prev[i] {
+                if curr[i] {
+                    info!("Button {} pressed", i + 1);
+                } else {
+                    debug!("Button {} released", i + 1);
+                }
+            }
+        }
+        prev = curr;
+
+        led1.set_level(if curr[0] { Level::Low } else { Level::High });
+        led2.set_level(if curr[1] { Level::Low } else { Level::High });
+        led3.set_level(if curr[2] { Level::Low } else { Level::High });
+        led4.set_level(if curr[3] { Level::Low } else { Level::High });
         Timer::after_millis(10).await;
     }
 }
