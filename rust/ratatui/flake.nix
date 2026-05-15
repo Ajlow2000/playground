96f602e11d15576e412d3633dcd5938621039b0f
Alec Lowry <accounts@aleclowry.com>
2026-05-15 09:25:46 -0600

    feat: hw for ratatui eventually

---
+++ rust/ratatui/flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Rust hello world with Cargo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system:
+        f (import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        })
+      );
+    in
+    {
+      packages = forAllSystems (pkgs:
+        let
+          toolchain = pkgs.rust-bin.nightly.latest.default;
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = toolchain;
+            rustc = toolchain;
+          };
+        in
+        {
+          default = rustPlatform.buildRustPackage {
+            pname = "hello";
+            version = "0.1.0";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+        }
+      );
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = [ pkgs.rust-bin.nightly.latest.default ];
+        };
+      });
+    };
+}
